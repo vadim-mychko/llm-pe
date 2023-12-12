40cab7f9e89d2318b31b65f6c43c815c00418331
David <aviday80@gmail.com>
2023-12-12 10:25:25 -0500

    redundant config calls fix

--- dialogue_sim.py
+++ dialogue_sim.py
@@ -12,10 +12,11 @@ class DialogueSimulator:
 
     def run_dialogue(self, user, pe_module):
         recs = []
+        num_top_items = self.config['dialogue_sim']['num_recs']
         for dialogue_turn_num in range(self.config['dialogue_sim']['num_turns']):
             query = pe_module.get_query()
             response = user.get_response(query)
             pe_module.update_from_response(query, response)
-            rec = pe_module.get_top_items(k=self.config['dialogue_sim']['num_recs'])
-            recs.append(rec) # TODO: This is slow, change for larger datasets
+            rec = pe_module.get_top_items(k=num_top_items)
+            recs.append(rec) 
         return recs
