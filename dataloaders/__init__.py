49967be15f2fa038293104f3560f1aad0ff0cff0
David <aviday80@gmail.com>
2023-12-21 16:30:43 -0500

    Experiment folder setup and fixes for full workflow.

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,9 +1,11 @@
 from dataloaders.dataloader import DataLoader
 from dataloaders.json_loader import JSONLoader
+from dataloaders.user_tsv_loader import UserTsvLoader
 
 # Used to create an instance of the appropriate data loader class based on the name provided in config.
 
 DATALOADER_CLASSES = {
     'DataLoader': DataLoader,
     'JSONLoader': JSONLoader,
+    'UserTsvLoader': UserTsvLoader,
 }
