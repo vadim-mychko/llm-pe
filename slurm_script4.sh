a0d52f265c7d73d83a875ead3cc87b672015a448
davaus80 <aviday80@gmail.com>
2024-01-15 14:10:15 -0500

    experiment results

---
+++ slurm_script4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=david_llm-pe_2
+#SBATCH --gres=gpu:1
+#SBATCH -c 2
+#SBATCH --mem=10G
+#SBATCH --output=./slurm_outputs/slurm-%j.out
+#SBATCH --error=./slurm_outputs/slurm-%j.err
+
+echo Running on $(hostname)
+date
+
+module load pytorch2.0-cuda11.8-python3.9
+module load cuda-11.8
+source activate llm-pe
+
+# (while true; do nvidia-smi; top -b -n 1 | head -20; sleep 10; done) &
+# srun accelerate launch /h/davaus80/McIntoshLab-MedBind/david_efficiency_test.py
+/h/davaus80/.conda/envs/llm-pe/bin/python3 /h/davaus80/llm-pe/experiment_manager.py -exp_dir=/h/davaus80/llm-pe/experiments/z_temp2
+# wait
