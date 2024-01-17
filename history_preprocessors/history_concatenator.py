5ceb89c1d0971445004196397bd9042d15365a89
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-01-17 12:13:37 -0500

    history concatenator update

--- history_preprocessors/history_concatenator.py
+++ history_preprocessors/history_concatenator.py
@@ -11,6 +11,6 @@ class HistoryConcatenator(HistoryPreprocessor):
     # Iterate over each interaction in the history
         for interaction in history: 
             # Append the 'query' and 'response' of each item to the string
-            concatenated_history += "Query: " + interaction['query'] + " \nResponse: " + interaction['response'] + " \n "
+            concatenated_history += " System: " + interaction['query'] + " \n User: " + interaction['response'] + " \n"
 
         return concatenated_history
