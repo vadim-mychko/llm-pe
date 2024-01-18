ffef74b7dd1e63a90ca40edf2e7da4d4698dc6f0
davaus80 <aviday80@gmail.com>
2024-01-17 21:07:31 -0500

    Some results

--- slurm_script5.sh
+++ slurm_script5.sh
@@ -15,5 +15,5 @@ source activate llm-pe
 
 # (while true; do nvidia-smi; top -b -n 1 | head -20; sleep 10; done) &
 # srun accelerate launch /h/davaus80/McIntoshLab-MedBind/david_efficiency_test.py
-/h/davaus80/.conda/envs/llm-pe/bin/python3 /h/davaus80/llm-pe/experiment_manager.py -exp_dir=/h/davaus80/llm-pe/experiments/jan_17_greedy_temp_sweep_llm_0_noise_0.5
+/h/davaus80/.conda/envs/llm-pe/bin/python3 /h/davaus80/llm-pe/experiment_manager.py -exp_dir=/h/davaus80/llm-pe/experiments/jan_17_recipe_greedy_temp_sweep_llm_0_noise_0
 # wait
