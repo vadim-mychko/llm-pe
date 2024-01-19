a9006c0b54c61ed6ffdedc1b12e6ae0668e6441d
davaus80 <aviday80@gmail.com>
2024-01-18 21:12:35 -0500

    Yelp cat att results

--- slurm_script3.sh
+++ slurm_script3.sh
@@ -15,5 +15,5 @@ source activate llm-pe
 
 # (while true; do nvidia-smi; top -b -n 1 | head -20; sleep 10; done) &
 # srun accelerate launch /h/davaus80/McIntoshLab-MedBind/david_efficiency_test.py
-/h/davaus80/.conda/envs/llm-pe/bin/python3 /h/davaus80/llm-pe/experiment_manager.py -exp_dir=/h/davaus80/llm-pe/experiments/jan_18_restaurants_100_er
+/h/davaus80/.conda/envs/llm-pe/bin/python3 /h/davaus80/llm-pe/experiment_manager.py -exp_dir=/h/davaus80/llm-pe/experiments/jan_18_yelp_cat_att_16_greedy
 # wait
